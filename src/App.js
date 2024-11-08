21edfbdef5b8220ce708fb63d721d822600d1d85
Rushikesh Gund <rushikeshgund@Rushikeshs-MacBook-Air.local>
2024-11-08 16:18:22 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Home/Home';
