800ee1a8facce061dafa199ab212e5d522d2ae6a
RSRAviationservices <it@rsraviation.com>
2025-01-06 13:44:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import NavBar from './Components/NavBar/NavBar';
@@ -15,10 +15,31 @@ import AboutUsMain from './Components/AboutUsMain/AboutUsMain';
 import Customers from './Components/Customers/Customers';
 
 const App = () => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  useEffect(() => {
+    setIsPopupVisible(true);
+  }, []);
+  const closePopup = () => {
+    setIsPopupVisible(false);
+  };
   return (
     <>
       <Header />
       <NavBar />
+      {isPopupVisible && (
+        <div className="popup-modal">
+          <div className="popup-content">
+            <img
+              src={posterImage} // Replace with the actual image path
+              alt="Welcome"
+              className="popup-image"
+            />
+            <button onClick={closePopup} className="popup-close-button">
+              X
+            </button>
+          </div>
+        </div>
+      )} 
       <Routes> 
         <Route path='/login' element={<Login />} />
         <Route path="/" element={<Home />} />
