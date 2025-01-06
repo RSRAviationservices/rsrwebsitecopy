0e12cbeef5495d84a2b8ceaea5f64f98aeac1d4e
RSRAviationservices <it@rsraviation.com>
2025-01-06 13:47:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Components/Header/Header';
-import NavBar from './Components/NavBar/NavBar';
-import Home from './Components/Home/Home';
-import Team from './Components/Team/Team';
-import  Products  from './Components/Products/Products';
-import Footer from './Components/Footer/Footer';
-import Login from './Components/login/login';
-import Cart from './Components/Cart/Cart';
-import ContactUs from './Components/ContactUs/ContactUs'
-import './App.css';
-import AboutUsMain from './Components/AboutUsMain/AboutUsMain';
-import Customers from './Components/Customers/Customers';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import NavBar from "./Components/NavBar/NavBar";
+import Home from "./Components/Home/Home";
+import Team from "./Components/Team/Team";
+import Products from "./Components/Products/Products";
+import Footer from "./Components/Footer/Footer";
+import Login from "./Components/login/login";
+import Cart from "./Components/Cart/Cart";
+import ContactUs from "./Components/ContactUs/ContactUs";
+import "./App.css";
+import AboutUsMain from "./Components/AboutUsMain/AboutUsMain";
+import Customers from "./Components/Customers/Customers";
 import posterImage from "./Resource/poster.svg";
 
 const App = () => {
   const [isPopupVisible, setIsPopupVisible] = useState(false);
+
   useEffect(() => {
     setIsPopupVisible(true);
   }, []);
+
   const closePopup = () => {
     setIsPopupVisible(false);
   };
@@ -40,20 +41,19 @@ const App = () => {
             </button>
           </div>
         </div>
-      )} 
-      <Routes> 
-        <Route path='/login' element={<Login />} />
+      )}
+      <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/Customers" element={<Customers/>}/>
-        <Route path="/aboutUs" element={<AboutUsMain/>}/>
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/Customers" element={<Customers />} />
+        <Route path="/aboutUs" element={<AboutUsMain />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/team" element={<Team />} />
         <Route path="/products" element={<Products />} />
         <Route path="/ContactUs" element={<ContactUs />} />
       </Routes>
       <Footer />
-     </>  
- 
+    </>
   );
 };
 
