53231a025f2617d7983a3c51b51603617c713e6d
RSRAviationservices <it@rsraviation.com>
2025-05-23 15:04:41 +0530

    Update Footer.jsx
    removed reach us from footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,41 +8,6 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-          {/* === Reach Us Section === */}
-        <div className="reach-us">
-          <h2>Reach Us</h2>
-          <div className="offices-container">
-            <div className="office-section">
-              <p>
-                <strong> Address:</strong> RSR AVIATION PVT.LTD UNIT NO B
-                -68-71,PLATINUM SPRING ,PLOT NO 6 G20 to G24 TALOJA,
-                PANVEL, MAHARASHTRA -410208
-              </p>
-              <p>
-                <strong> Contact Person:</strong> SURESH RANHAGDALE{" "}
-              </p>
-              <p>
-                <strong> For Corporate and Business-related Queries:</strong>{" "}
-                +91 9595475981
-              </p>
-
-              <p>
-                <strong> For HR related Queries:</strong>{" "}
-                <a href="mailto:hr@rsraviation.com">HR@RSRAVIATION.COM</a>
-              </p>
-              <p>
-                <strong> Email:</strong>{" "}
-                <a href="mailto:info@rsraviation.com">INFO@RSRAVIATION.COM</a>
-              </p>
-            </div>
-          </div>
-
-          <div className="business-hours">
-            <h3>Business Hours</h3>
-            <p>🕒 MONDAY – SATURDAY - 10AM to 6PM</p>
-          </div>
-        </div>
-
         <div className="social-links">
           <a href="https://www.instagram.com/rsr_aviation/profilecard/?igsh=MWFsNHB1bjB4ZnQ2" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
             <FontAwesomeIcon icon={faInstagram} />
